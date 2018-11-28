7947415007b56c44cce2bfca9d8995d2db85ac65
akim <akim_delli@hotmail.com>
2018-11-27 22:51:06 -0500

    Constraint-Satisfaction Problem: constraint base class

---
+++ Constraint-satisfaction problems/csp.py
@@ -0,0 +1,17 @@
+from typing import Generic, TypeVar, Dict, List, Optional
+from abc import ABC, abstractmethod
+
+# variable Type
+V = TypeVar('V')
+# domain type
+D = TypeVar('D')
+
+# Base class for all constraints
+class Constraints(Generic[V, D], ABC):
+    # the variables that the constraint is between
+    def __init__(self, variables: List[V]) -> None:
+        self.variables = variables
+
+    # Must be overridden by subclasses
+    @abstractmethod
+    def satisfied(self, assignement: Dict[V, D]) -> bool:
