ba2ccf2b9c46a74c60c794d8f08bff3f3cb5c6b0
akim <akim_delli@hotmail.com>
2018-12-02 00:40:29 -0500

    implement CSP framework

--- Constraint-satisfaction problems/csp.py
+++ Constraint-satisfaction problems/csp.py
@@ -8,7 +8,7 @@ D = TypeVar('D')
 
 
 # Base class for all constraints
-class Constraints(Generic[V, D], ABC):
+class Constraint(Generic[V, D], ABC):
     # the variables that the constraint is between
     def __init__(self, variables: List[V]) -> None:
         self.variables = variables
@@ -17,3 +17,35 @@ class Constraints(Generic[V, D], ABC):
     @abstractmethod
     def satisfied(self, assignment: Dict[V, D]) -> bool:
         pass
+
+
+# A constraint satisfaction problem consists of variables of type V
+# that have ranges of values known as domains of type D and contraints
+# that determines wether a particular variable's domain selection is valid
+class CSP(Generic[V, D]):
+    def __init__(self, variables: List[V], domains: Dict[V, List[D]]) -> None:
+        # variables to be constrained
+        self.variables: List[V] = variables
+        # domain for each variable
+        self.domains: Dict[V, List[D]] = domains
+        self.constraints: Dict[V, List[Constraint[V, D]]] = {}
+        for variable in self.variables:
+            self.constraints[variable] = []
+            if variable not in self.domains:
+                raise LookupError("Every variable should have a doamin assigned to it.")
+
+    def add_constraint(self, constraint: Constraint[V, D]) -> None:
+        for variable in constraint.variables:
+            if variable not in self.variables:
+                raise LookupError("Variable in constraint not in CSP")
+            else:
+                self.constraints[variable].append(constraint)
+
+    # Check if the value assignement is consistent by checking all constraints
+    # for the given variable against it
+    def consistent(self, variable: V, assignement: Dict[V, D]) -> bool:
+        for constraint in self.contraints[variable]:
+            if not constraint.satistfied(assignement):
+                return False
+        return True
+
