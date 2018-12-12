9322852d85cc2be9994a6821d7c5d111ee562cd9
Akim Delli <adelli@hearst.com>
2018-12-12 16:11:09 -0500

    CSP : eight queens problem

---
+++ Constraint-satisfaction problems/queens.py
@@ -0,0 +1,20 @@
+from csp import Constraint, CSP
+from typing import Dict, List, Optional
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    columns: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
+    rows: Dict[int, List[int]] = {}
+    for column in columns:
+        rows[column] = [1, 2, 3, 4, 5, 6, 7, 8]
+    csp: CSP[int, int] = CSP(columns, rows)
